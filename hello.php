19560fac65cd3a00ea6acd3e9907a52a2c6befd7
Vic Metcalfe <vic@zymsys.com>
2012-03-29 19:24:02 -0400

    Applied SRP to Hello World.  Code is much easier to understand and maintain now.

--- hello.php
+++ hello.php
@@ -1,2 +1,35 @@
 <?php
-echo "Hello World!\n";
+class GreetingModel
+{
+    private $_greeting;
+
+    function __construct()
+    {
+        $this->_greeting = "Hello World!";
+    }
+
+    public function getGreeting()
+    {
+        return $this->_greeting;
+    }
+}
+
+class GreetingView
+{
+    public function issueGreeting(GreetingModel $model)
+    {
+        echo $model->getGreeting() . "\n";
+    }
+}
+
+class GreetingController
+{
+    public function takeAction()
+    {
+        $view = new GreetingView();
+        $view->issueGreeting(new GreetingModel());
+    }
+}
+
+$controller = new GreetingController();
+$controller->takeAction();
